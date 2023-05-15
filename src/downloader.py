297ea5383fe21917399e4b9260dc9c96dd39645f
Tom <tombk@web.de>
2023-05-15 21:48:24 +0200

    update requirements and downloader

--- src/downloader.py
+++ src/downloader.py
@@ -13,19 +13,18 @@ class YouTubeMp3Downloader:
         
     def download(self):
         """
-            download mp4 file from youtube 
-            mp4 file will be stored in video folder
-            mp4 file will be converted into mp3 and stored in music with self.name
+        download mp4 file from youtube 
+        mp4 file will be stored in video folder
+        mp4 file will be converted into mp3 and stored in music with self.name
         """
         # download file mp4
         ssl._create_default_https_context = ssl._create_unverified_context
-        youtubeObj = YouTube(self.url_link)
-        youtubeObj = youtubeObj.streams.get_audio_only()
-        youtubeObj.download("audio_content")
+        youtube_obj = YouTube(self.url_link)
+        audio_streams = youtube_obj.streams.filter(only_audio=True)
+        audio_streams[0].download(output_path="audio_content")
         
         # download the thumbnail
-        youtubeObj = YouTube(self.url_link)
-        thumbnail_url = youtubeObj.thumbnail_url
+        thumbnail_url = youtube_obj.thumbnail_url
         urllib.request.urlretrieve(thumbnail_url, "thumbnail.jpg")
         
         # convert to mp3        
@@ -37,10 +36,9 @@ class YouTubeMp3Downloader:
         mp3_file = f"audio_content/{self.name}.mp3"
         
         # Convert to mp3
-        videoClip = AudioFileClip(mp4_file_location)
-        videoClip.write_audiofile(mp3_file, bitrate = "192k")
-        videoClip.close()
+        video_clip = AudioFileClip(mp4_file_location)
+        video_clip.write_audiofile(mp3_file, bitrate="192k")
+        video_clip.close()
         
-        # Delte MP4 file
+        # Delete MP4 file
         os.remove(mp4_file_location)
-        
