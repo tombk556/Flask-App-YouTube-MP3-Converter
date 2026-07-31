e2885a6aa49c855b7f33342f95bd098ceee21656
Tom Bischopink <s87622@informatik.htw-dresden.de>
2026-07-31 12:04:06 +0200

    update downloader to use yt-dlp for audio extraction and improve requirements

--- src/downloader.py
+++ src/downloader.py
@@ -1,44 +1,38 @@
-from pytube import YouTube
-import ssl
-from moviepy.editor import *
-from moviepy.audio.io.AudioFileClip import AudioFileClip
-from os import listdir
-from os.path import isfile, join
+import os
 import urllib.request
+import yt_dlp
+
+
 class YouTubeMp3Downloader:
     def __init__(self, url_link: str, name: str, ) -> None:
-        
+
         self.url_link = url_link
         self.name = name
-        
+
     def download(self):
         """
-        download mp4 file from youtube 
-        mp4 file will be stored in video folder
-        mp4 file will be converted into mp3 and stored in music with self.name
+        download audio from youtube and convert it into mp3,
+        stored in audio_content with self.name
         """
-        # download file mp4
-        ssl._create_default_https_context = ssl._create_unverified_context
-        youtube_obj = YouTube(self.url_link)
-        audio_streams = youtube_obj.streams.filter(only_audio=True)
-        audio_streams[0].download(output_path="audio_content")
-        
-        # download the thumbnail
-        thumbnail_url = youtube_obj.thumbnail_url
-        urllib.request.urlretrieve(thumbnail_url, "thumbnail.jpg")
-        
-        # convert to mp3        
-        # get the mp4 file name in the /video folder
-        mypath = "audio_content"
-        mp4_file_name = [f for f in listdir(mypath) if isfile(join(mypath, f))][0]
-        
-        mp4_file_location = f"audio_content/{mp4_file_name}"
+        os.makedirs("audio_content", exist_ok=True)
         mp3_file = f"audio_content/{self.name}.mp3"
-        
-        # Convert to mp3
-        video_clip = AudioFileClip(mp4_file_location)
-        video_clip.write_audiofile(mp3_file, bitrate="192k")
-        video_clip.close()
-        
-        # Delete MP4 file
-        os.remove(mp4_file_location)
+
+        ydl_opts = {
+            "format": "bestaudio/best",
+            "outtmpl": f"audio_content/{self.name}.%(ext)s",
+            "postprocessors": [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }],
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(self.url_link, download=True)
+
+        # download the thumbnail
+        thumbnail_url = info.get("thumbnail")
+        if thumbnail_url:
+            urllib.request.urlretrieve(thumbnail_url, "thumbnail.jpg")
+
+        return mp3_file
